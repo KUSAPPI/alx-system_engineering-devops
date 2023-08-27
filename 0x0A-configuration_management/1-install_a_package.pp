923571b2e04fcbe9ee41476261226913901847cd
Bright Kusi Appiah <appiahkusibright@gmail.com>
2023-08-27 14:06:06 -0700

    Task Completed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,21 +3,3 @@ package { 'puppet-lint':
   ensure   => '2.1.1',
   provider => 'gem',
 }
-
-# Define a Puppet class for installing Flask
-class flask_install {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  # Use pip3 to install Flask version 2.1.0
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    path    => ['/usr/bin'],
-    creates => '/usr/local/lib/python3.8.10/dist-packages/flask',
-    require => Package['python3-pip'],
-  }
-}
-
-# Apply the class to install Flask
-include flask_install
